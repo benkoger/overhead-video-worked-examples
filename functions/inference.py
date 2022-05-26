9cc3c92458139f4d9c3b1c046bceb7c46dd0e32a
Ben Koger <benkoger@gmail.com>
2022-05-26 14:18:52 +0200

    Clean inference notebooks.

--- functions/inference.py
+++ functions/inference.py
@@ -1,4 +1,5 @@
 from detectron2.data import DatasetCatalog
+from torchvision.ops import nms
 
 def load_inference_instances(image_files):
     """ Use all given files to make Detectron2 format list of dicts."""
@@ -18,3 +19,19 @@ def register_inference_instances(name, image_files):
         image_files: list of image_files
     """
     DatasetCatalog.register(name, lambda: load_inference_instances(image_files))
+
+def nms_all_classes(instances, iou_thresh):
+    """ Apply non-maximum suppression to inference instances regardless of class.
+    
+    Args:
+        instances: instances from detectron2 model
+        iou_thresh: threshold to use for nms
+        
+    returns resulting instances after nms is applied
+    """
+    valid_ind = nms(instances.pred_boxes.tensor, instances.scores, iou_thresh)
+    instances.pred_boxes.tensor = instances.pred_boxes.tensor[valid_ind]
+    instances.scores = instances.scores[valid_ind]
+    instances.pred_classes = instances.pred_classes[valid_ind]
+    
+    return instances
